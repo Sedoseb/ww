1fc9639fccca0dbc3195baf8369238fd05281f70
Dona <116895472+Sedoseb@users.noreply.github.com>
2025-01-19 19:30:23 +0530

    Update wrapper.dart

--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ww/View/pages/home.dart';
 import 'package:ww/View/pages/login.dart';
-// Ensure SplashScreen class is defined properly
 
 class Wrapper extends StatefulWidget {
   const Wrapper({super.key});
